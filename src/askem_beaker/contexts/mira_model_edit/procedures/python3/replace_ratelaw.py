a16ce5c6a0224d32f3e6a403bcfd29411bf42700
Daniel Chang <mwdchang@gmail.com>
2024-08-07 09:57:56 -0400

    add states to local dict for sympy

--- src/askem_beaker/contexts/mira_model_edit/procedures/python3/replace_ratelaw.py
+++ src/askem_beaker/contexts/mira_model_edit/procedures/python3/replace_ratelaw.py
@@ -1,3 +1,4 @@
+import sympy
 def replace_rate_law_sympy(model, template_name, new_rate_law):
     """Replace the rate law of transition. The new rate law passed in will be a sympy.Expr object
 
@@ -8,8 +9,6 @@ def replace_rate_law_sympy(model, template_name, new_rate_law):
         typically the name of the transition
     new_rate_law :
         The new rate law to replace the existing rate law with
-    local_dict :
-        A dictionary of local variables to use when parsing.
 
     Returns
     -------
@@ -18,7 +17,10 @@ def replace_rate_law_sympy(model, template_name, new_rate_law):
     """
     assert isinstance(model, TemplateModel)
     tm = model
-    local_dict = { key:key for key, _value in tm.parameters.items() }
+    local_dict = { key:sympy.Symbol(key) for key, _value in tm.parameters.items() }
+    for k, v in mmt.get_concepts_name_map().items():
+        local_dict[k] = sympy.Symbol(k)
+
     for template in tm.templates:
         if template.name == template_name:
             template.set_rate_law(new_rate_law, local_dict=local_dict)
