ba801d84ecbed15020016af4df27f29db403f4aa
Brandon Rose <rose.brandon.m@gmail.com>
2024-12-20 08:46:01 -0600

    suppress chained pandas warnings

--- src/askem_beaker/contexts/dataset/procedures/python3/setup.py
+++ src/askem_beaker/contexts/dataset/procedures/python3/setup.py
@@ -1,7 +1,9 @@
 import pandas as pd; import numpy as np; import scipy; import pickle
+import warnings
 
 import IPython
 formatter = IPython.get_ipython().display_formatter.formatters['text/plain']
 formatter.max_seq_length = 0
 
 pd.options.mode.chained_assignment = None
+warnings.filterwarnings('ignore')
